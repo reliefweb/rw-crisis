828d0381eb888c909849b147b9e9c419d6c52bd4
Grayside <aross@phase2technology.com>
2015-01-21 20:07:01 -0800

    Add CORS support to grunt serve task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,16 @@ module.exports = function (grunt) {
           base: 'src',
           port: 9001,
           open: true,
-          keepalive: true
+          keepalive: true,
+          middleware: function (connect, options, middlewares) {
+            middlewares.unshift(function(req, res, next) {
+              res.setHeader('Access-Control-Allow-Origin', '*');
+              res.setHeader('Access-Control-Allow-Methods', '*');
+              next();
+            });
+
+            return middlewares;
+          }
         }
       }
     },
