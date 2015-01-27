24091a3810202e42dcb2183e7d0e98737822859b
Brandon Morrison <brandon@brandonmorrison.com>
2015-01-26 22:02:59 -0500

    Load widgets as iframes into crisis page. Needs better height management

--- config/config.js
+++ config/config.js
@@ -1,13 +1,30 @@
 // This file is used to process the config option for each widget. A grunt task
 // called browserfy is then used to "browserfy" this file and then copy it to
 // the src directory.
+
+// Dependency: oembed-client-cors
 var config = require("./config.json");
 var widgets = config.widgets;
 
-widgets.forEach(function(widget){
-  var widgetDiv = "#" + widget.key;
-  var widgetConfig = widget.config;
-  widgetConfig.template = Handlebars.templates[widgetConfig.template];
-  var newWidget = ReliefwebWidgets.widget(widget.type, widgetConfig);
-  newWidget.render(widgetDiv);
+// var $ = jQuery;
+// var _ = lodash;
+
+var oembed_server = config['oembed-server'];
+//var oembed_server = "http://localhost:3000";
+
+//var self_domain = window.location.host + '/';
+var self_domain = '';
+
+widgets.forEach(function(widget) {
+  var oembed_url = oembed_server + '/v0/oembed/' + widget.slug + '?' + $.param(widget.config);
+
+  if (widget.slug == 'timeline') {
+    $('#' + widget.slug).html('<iframe class="oembed" src="' + oembed_server + '/v0/widgets/timeline?country[]=Iraq&country[]=Syria' + '" height="800px"></iframe>');
+  } else {
+    $.get(oembed_url, '', function(data) {
+      var $markup = $(decodeURI(data.html));
+      $markup.addClass('oembed');
+      $("#" + widget.slug).html($markup);
+    }, 'json');
+  }
 });
