3b3a243a61daa0c7f03fdf25de895c0bb83b63c0
Grayside <aross@phase2technology.com>
2015-04-27 13:11:18 -0700

    UNRW-779: Align nginx and docker startup with production behavior.

--- env/stage/opt/app/node.sh
+++ env/stage/opt/app/node.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+##
+# Set up node application inside docker container.
+#
+# Takes 2 arguments.
+# * If one argument --local or a URL to download as main config.
+# * If two arguments, the first is a flag and the second is a URL.
+##
+
+URL=''
+LOCAL=''
+args=("$@")
+if [ $# -gt 0 ] && [ ${args[0]} == '--local' ]
+  then LOCAL='--local'
+fi
+if [ $# -eq 1 ] && [ ${args[0]} != '--local' ]
+  then URL=${args[0]}
+elif [ $# -eq 2 ]
+  then URL=${args[1]}
+fi
+
+if [ -n "$URL" ]; then
+  curl $URL > /var/www/html/config/config.json
+fi
 
 # Manually active ruby193.
 export PATH="/opt/rh/ruby193/root/usr/bin:${PATH}"
@@ -18,6 +41,4 @@ echo "==> Installing npm dependencies"
 npm install --unsafe-perm
 
 echo "==> Build embed assets and run tests"
-grunt
-
-grunt watch
+grunt $LOCAL
